a2c83499818dd29c8d44ed48217c1b0c37694cd6
iNFiENiTE-World <infienite@gmail.com>
2022-03-21 00:06:17 +0800

    Server Express dibangunkan

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var createServer = require('http').createServer;
-var Server = createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!');
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Listened")
+})
+
+app.get('/', (req, res) => {
+    res.writeHead(200, {"Content-Type":"text/html"});
+    res.write("Hello, User!");
     res.end();
-});
-Server.listen(3001);
+})
