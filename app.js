7890f2e8c7006ecaca7dc626712689fea08d6d43
iNFiENiTE-World <infienite@gmail.com>
2022-03-24 23:16:57 +0800

    Berjaya mengimplementasikan router dalam server!

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
-const { readFileSync } = require('fs');
 const express = require("express");
 const app = express();
 const indexRoute = require('./routes/index');
 
-const routeLog = (req, res, next) => {
-    console.log(`URL: ${req.url}\nBody: ${req.body}\nParams: ${req.params}`)
-    next();
-}
-
-// Menetapkan port pendengaran server
+// Penetapan port pendengaran server
 app.listen(3000, () => {
     console.log("Listened")
 })
 
-/*
-// Menghantar laman utama kepada pengguna
-app.get('/', (req, res) => {
-    res.writeHead(200, {"Content-Type":"text/html"});
-    const main = readFileSync("../client/index.html");
-    res.write(main);
-    res.end();
-})
-*/
+// Penetapan router yang digunakan
 app.use('/', indexRoute);
-
-/* Menghantar semua jenis fail JS yang dipesan oleh pengguna
-• fail JS yang dihantar adalah sebahagian daripada skrip HTML
-*/
-app.get('/*.js', routeLog, (req, res) => {
-    res.writeHead(200, {"Content-Type":"text/html"});
-    const main = readFileSync(`../client${req.url}`);
-    res.write(main);
-    res.end();
-})
