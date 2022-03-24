e0c6b08a43e08c5022dd399233f030f56b85a911
iNFiENiTE-World <infienite@gmail.com>
2022-03-24 15:18:56 +0800

    Fungsi generik untuk mengendali segala pesanan fail skrip JS oleh pengguna telah diaturcara

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const { readFileSync } = require('fs');
 const express = require("express");
 const app = express();
 
+const routeLog = (req, res, next) => {
+    console.log(`URL: ${req.url}\nBody: ${req.body}\nParams: ${req.params}`)
+    next();
+}
+
+// Menetapkan port pendengaran server
 app.listen(3000, () => {
     console.log("Listened")
 })
 
+// Menghantar laman utama kepada pengguna
 app.get('/', (req, res) => {
     res.writeHead(200, {"Content-Type":"text/html"});
     const main = readFileSync("../client/index.html");
@@ -13,10 +20,12 @@ app.get('/', (req, res) => {
     res.end();
 })
 
-app.get('/index.js', (req, res) => {
+/* Menghantar semua jenis fail JS yang dipesan oleh pengguna
+• fail JS yang dihantar adalah sebahagian daripada skrip HTML
+*/
+app.get('/*.js', routeLog, (req, res) => {
     res.writeHead(200, {"Content-Type":"text/html"});
-    const main = readFileSync("../client/index.js");
+    const main = readFileSync(`../client${req.url}`);
     res.write(main);
     res.end();
 })
-
