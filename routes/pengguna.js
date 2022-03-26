f894685acf2886c10e44900c1f3a9b3f787fd035
iNFiENiTE-World <infienite@gmail.com>
2022-03-26 08:56:52 +0800

    Pengendali penciptaan akaun baharu pada url '/pengguna' jenis 'post' dibuat

--- routes/pengguna.js
+++ routes/pengguna.js
@@ -1,26 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const { MongoClient } = require("mongodb");
+const { readFileSync } = require('fs');
 
-const url = `mongodb+srv://iNFiENiTE:${require('../‎')}@infienite-cluster.4j1az.mongodb.net/myfirstmongodb?retryWrites=true&w=majority`;
+// Pendirian pangkalan data
+const url = `mongodb+srv://iNFiENiTE:${require('../‎')}@infienite-cluster.4j1az.mongodb.net/urusmarkah?retryWrites=true&w=majority`;
 const pangkalan_data = new MongoClient(url);
 
+// Menyambung hubungan dengan pangkalan data
 const hubunganPangkalanData = async (req, res, next) => {
     try {
         await pangkalan_data.connect();
         console.log("Hubungan berjaya!")
-        next();
+        await next();
     }
     catch (err) {
         console.log(err.stack)
     }
-}
+};
 
-router.use(hubunganPangkalanData)
+// Penetapan middleware
+router.use(hubunganPangkalanData);
+router.use(express.urlencoded());
 
-
+// Dapatkan halaman borang
 router.get("/", (req, res) => {
-    res.end("USER ACCESSED!");
+    res.writeHead(200, {"Content-Type":"text/html"});
+    const borang = readFileSync("../client/borang.html");
+    res.write(borang);
+    res.end();
+});
+
+// Cipta akaun pengguna baharu
+router.post("/", (req, res) => {
+    const koleksi = pangkalan_data.db().collection("pengguna");
+    const maklumatAkaunBaharu = { emel: req.body.emel, nama: req.body.nama }
+    koleksi.insertOne(maklumatAkaunBaharu);
+    res.end();
 });
 
 module.exports = router;
