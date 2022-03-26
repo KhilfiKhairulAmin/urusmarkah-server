0f62ac9a317232eb2782e8b2e3ed2eb08911441e
iNFiENiTE-World <infienite@gmail.com>
2022-03-26 09:13:19 +0800

    Redirect ke halaman utama selepas borang dihantar dibuat

--- routes/pengguna.js
+++ routes/pengguna.js
@@ -8,7 +8,7 @@ const url = `mongodb+srv://iNFiENiTE:${require('../‎')}@infienite-cluster.4j1a
 const pangkalan_data = new MongoClient(url);
 
 // Menyambung hubungan dengan pangkalan data
-const hubunganPangkalanData = async (req, res, next) => {
+const hubungPangkalanData = async (req, res, next) => {
     try {
         await pangkalan_data.connect();
         console.log("Hubungan berjaya!")
@@ -20,7 +20,7 @@ const hubunganPangkalanData = async (req, res, next) => {
 };
 
 // Penetapan middleware
-router.use(hubunganPangkalanData);
+router.use(hubungPangkalanData);
 router.use(express.urlencoded());
 
 // Dapatkan halaman borang
@@ -32,11 +32,11 @@ router.get("/", (req, res) => {
 });
 
 // Cipta akaun pengguna baharu
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     const koleksi = pangkalan_data.db().collection("pengguna");
     const maklumatAkaunBaharu = { emel: req.body.emel, nama: req.body.nama }
-    koleksi.insertOne(maklumatAkaunBaharu);
-    res.end();
+    await koleksi.insertOne(maklumatAkaunBaharu);
+    res.redirect(301, "http://localhost:3000/");
 });
 
 module.exports = router;
