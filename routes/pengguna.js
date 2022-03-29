328a9762200dbe446599387e4fbcff2f3393b53f
iNFiENiTE-World <infienite@gmail.com>
2022-03-29 21:21:25 +0800

    Router dioptimumkan

--- routes/pengguna.js
+++ routes/pengguna.js
@@ -3,25 +3,6 @@ const router = express.Router();
 const { MongoClient } = require("mongodb");
 const { readFileSync } = require('fs');
 
-// Pendirian pangkalan data
-const url = `mongodb+srv://iNFiENiTE:${require('../‎')}@infienite-cluster.4j1az.mongodb.net/urusmarkah?retryWrites=true&w=majority`;
-const pangkalan_data = new MongoClient(url);
-
-// Menyambung hubungan dengan pangkalan data
-const hubungPangkalanData = async (req, res, next) => {
-    try {
-        await pangkalan_data.connect();
-        console.log("Hubungan berjaya!")
-        await next();
-    }
-    catch (err) {
-        console.log(err.stack)
-    }
-};
-
-// Penetapan middleware
-router.use(hubungPangkalanData);
-router.use(express.urlencoded());
 
 // Dapatkan halaman borang
 router.get("/", (req, res) => {
@@ -31,12 +12,33 @@ router.get("/", (req, res) => {
     res.end();
 });
 
+
+// Pendirian pangkalan data
+const url = `mongodb+srv://iNFiENiTE:${require('../‎')}@infienite-cluster.4j1az.mongodb.net/urusmarkah?retryWrites=true&w=majority`;
+const pangkalan_data = new MongoClient(url);
+
+// Menyambung hubungan dengan pangkalan data
+const hubungPangkalanData = async (req, res, next) => {
+    try {
+        await pangkalan_data.connect();
+        console.log("Hubungan berjaya!");
+        await next().then(pangkalan_data.close())
+    }
+    catch (err) {
+        console.log(err.stack)
+    }
+};
+
+// Penetapan middleware bagi penggunaan pangkalan data
+router.use(express.urlencoded())
+router.use(hubungPangkalanData);
+
 // Cipta akaun pengguna baharu
 router.post("/", async (req, res) => {
-    const koleksi = pangkalan_data.db().collection("pengguna");
+    const koleksi = pangkalan_data.db("urusmarkah").collection("pengguna");
     const maklumatAkaunBaharu = { emel: req.body.emel, nama: req.body.nama }
     await koleksi.insertOne(maklumatAkaunBaharu);
-    res.redirect(301, "http://localhost:3000/");
+    res.redirect(301, "http://localhost:3000");
 });
 
 module.exports = router;
