aef147706c75d4996ade16227d2da0b12c7e2e17
iNFiENiTE-World <infienite@gmail.com>
2022-04-03 11:22:09 +0800

    Route difaktorkan selengkapnya

--- routes/pengguna.js
+++ routes/pengguna.js
@@ -1,32 +1,7 @@
-const express = require('express');
-const router = express.Router();
-const { MongoClient } = require("mongodb");
-const { readFileSync } = require('fs');
-
-
-// Dapatkan halaman
-router.get("/", (req, res) => {
-    if (req.query.emel) {
-        res.writeHead(200, {"Content-Type":"text/html"});
-        const utama = readFileSync("../client/pages/utama.html");
-        res.write(utama);
-        res.end();
-    }
-    else {
-        res.writeHead(200, {"Content-Type":"text/html"});
-        const borang = readFileSync("../client/pages/borang.html");
-        res.write(borang);
-        res.end();
-    }
-});
-
-
-router.get("/log_masuk", (req, res) => {
-    res.writeHead(200, {"Content-Type":"text/html"});
-    const borang = readFileSync("../client/pages/log_masuk.html");
-    res.write(borang);
-    res.end();
-})
+const express = require('express')
+const router = express.Router()
+const { MongoClient } = require("mongodb")
+const { readFileSync } = require('fs')
 
 
 // Pendirian pangkalan data
@@ -43,24 +18,45 @@ const hubungPangkalanData = async (req, res, next) => {
     catch (err) {
         console.log(err.stack)
     }
-};
+}
 
-// Penetapan middleware bagi penggunaan pangkalan data
-router.use(express.urlencoded())
-router.use(hubungPangkalanData);
+// Penetapan middleware bagi kaedah permintaan POST
+router.post('*', hubungPangkalanData, express.urlencoded())
 
-// Cipta akaun pengguna baharu
-router.post("/", async (req, res) => {
+// Dapatkan halaman
+router.route('/')
+.get((req, res) => {
+    if (req.query.emel) {
+        res.writeHead(200, {"Content-Type":"text/html"});
+        const utama = readFileSync("../client/pages/utama.html");
+        res.write(utama);
+        res.end();
+    }
+    else {
+        res.writeHead(200, {"Content-Type":"text/html"});
+        const borang = readFileSync("../client/pages/borang.html");
+        res.write(borang);
+        res.end();
+    }
+})
+.post(async (req, res) => { // Cipta akaun pengguna baharu
     const koleksi = pangkalan_data.db().collection("pengguna");
     const maklumatAkaunBaharu = { emel: req.body.emel, nama: req.body.nama }
     await koleksi.insertOne(maklumatAkaunBaharu);
     res.redirect(301, "http://localhost:3000");
 });
 
-router.post("/log_masuk", async (req, res) => {
-    const koleksi = pangkalan_data.db().collection("pengguna");
-    const maklumatAkaun = { emel: req.body.emel };
-    const carian = await koleksi.findOne(maklumatAkaun);
+router.route('log_masuk')
+.get((req, res) => {
+    res.writeHead(200, {"Content-Type":"text/html"})
+    const borang = readFileSync("../client/pages/log_masuk.html")
+    res.write(borang)
+    res.end()
+})
+.post(async (req, res) => {
+    const koleksi = pangkalan_data.db().collection("pengguna")
+    const maklumatAkaun = { emel: req.body.emel }
+    const carian = await koleksi.findOne(maklumatAkaun)
     if (carian.emel != undefined) {
         res.redirect(301, `http://localhost:3000/pengguna?emel=${carian.emel}`)
     }
@@ -69,7 +65,8 @@ router.post("/log_masuk", async (req, res) => {
     }
 });
 
-router.get("/utama", async (req, res) => {
+router.route('/utama')
+.get(async (req, res) => {
     const koleksi = pangkalan_data.db().collection("pengguna");
     const maklumat = { emel: req.query.emel };
     const carian = await koleksi.findOne(maklumat);
