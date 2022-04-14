3c6a0c45fe8aaba550dd2405d8416c7ddf830864
iNFiENiTE-World <infienite@gmail.com>
2022-04-14 18:35:51 +0800

    URL untuk ke API pengguna dikemaskini

--- aplikasi.js
+++ aplikasi.js
@@ -6,6 +6,6 @@ const indexRoute = require('./api/v1/index');
 aplikasi.use('/', indexRoute);
 
 const penggunaRoute = require('./api/v1/pengguna');
-aplikasi.use('/pengguna', penggunaRoute);
+aplikasi.use('/api/v1/pengguna', penggunaRoute);
 
 module.exports = aplikasi;
