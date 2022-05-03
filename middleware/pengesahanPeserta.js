ca3760884b659931d32973daa5a2cbf9818f3560
iNFiENiTE-World <infienite@gmail.com>
2022-05-04 07:31:51 +0800

    Middleware untuk pengesahan kewujudan peserta

---
+++ middleware/pengesahanPeserta.js
@@ -0,0 +1,26 @@
+const Peserta = require("../model/Peserta");
+
+const pengesahanPeserta = async (req, res, next) => {
+    try {
+        const { peserta_id } = req.params;
+
+        if (!peserta_id) {
+            res.status(400).send({ mesej: 'Sila berikan ID Peserta' });
+        }
+    
+        const peserta = await Peserta.findById(peserta_id);
+    
+        if (!peserta) {
+            res.status(404).send({ mesej: 'Peserta tidak wujud' });
+        }
+    
+        req.peserta = peserta;
+
+        next();
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+}
+
+module.exports = pengesahanPeserta;
