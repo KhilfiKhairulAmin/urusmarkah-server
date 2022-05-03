03e09cd90ba83efdebdf456787bd8024afac08fb
iNFiENiTE-World <infienite@gmail.com>
2022-05-04 07:33:42 +0800

    Route Peserta dibuat. Operasi CRUD telah diimplementasikan: GET semua GET satu POST cipta banyak PUT satu DELETE banyak

--- api/peserta.js
+++ api/peserta.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pengesahanPeserta = require('../middleware/pengesahanPeserta');
 const Peserta = require('../model/Peserta');
 const router = express.Router();
 
@@ -38,5 +39,86 @@ router.post('/daftar', async (req, res) => {
         res.status(500).end();
     }
 });
+
+router.get('/', async (req, res) => {
+    try {
+        const { pertandingan } = req;
+
+        const semuaPeserta = await Peserta.find({ pertandingan_id: pertandingan._id });
+
+        res.status(200).send(semuaPeserta);
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
+router.get ('/:peserta_id', pengesahanPeserta, async (req, res) => {
+    try {
+        const { peserta } = req;
+        
+        return res.status(200).send(peserta);
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
+router.put('/:peserta_id/kemas_kini', pengesahanPeserta, async (req, res) => {
+    try {
+        const { peserta } = req;
+        const { nama_peserta } = req.body;
+
+        if (!nama_peserta) {
+            return res.status(400).send({ mesej: 'Sila berikan nama peserta' });
+        }
+
+        peserta.nama_peserta = nama_peserta;
+
+        peserta.save();
+
+        res.status(200).send(peserta);
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
+router.delete('/hapus', async (req, res) => {
+    try {
+        const { peserta_id } = req.body;
+        
+        if (!peserta_id) {
+            res.status(400).send({ mesej: 'Sila berikan ID Peserta' });
+        }
+
+        if (!Array.isArray(peserta_id)) {
+            return res.status(400).send({ mesej: 'Sila letakkan peserta dalam Array' });
+        }
+
+        let i = 1;
+
+        for (const id of peserta_id) {
+
+            if (!id) {
+                return res.status(400).send({ mesej: `Sila letakkan ID Peserta pada peserta ${i}` });
+            }
+
+            if (typeof id !== 'string') {
+                return res.status(400).send({ mesej: `Sila letakkan ID dalam format string pada peserta ${i}` });
+            }
+
+            i++;
+        }
+
+        const { deletedCount } = await Peserta.deleteMany({ _id: peserta_id });
+
+        res.status(200).send({ mesej: `${deletedCount} peserta telah dihapuskan` });
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
 
 module.exports = router;
