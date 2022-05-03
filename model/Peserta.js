da0b9c7d907ba88ed69769f96b6e7e72637a9344
iNFiENiTE-World <infienite@gmail.com>
2022-05-04 07:31:06 +0800

    Markah dan Peserta diasingkan kepada dua skema berbeza

--- model/Peserta.js
+++ model/Peserta.js
@@ -3,10 +3,6 @@ const { Schema, model } = require('mongoose');
 const skemaPeserta = new Schema({
     pertandingan_id: { type: String, required: true },
     nama_peserta: { type: String, required: true },
-    maklumat_tambahan: {
-        kunci_unik_peserta: { type: String },
-        nota: { type: String }
-    }
 });
 
 module.exports = model('peserta', skemaPeserta, 'peserta');
