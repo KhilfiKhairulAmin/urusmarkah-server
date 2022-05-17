5f39cf2a22f762860a57b0e73d1f18fad02804a2
iNFiENiTE-World <infienite@gmail.com>
2022-05-17 20:16:11 +0800

    Model validasi dikemaskini dengan default value bagi resfresh token

--- model/Validasi.js
+++ model/Validasi.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const skemaValidasi = new Schema ({
     pengguna_id: { type: String, unique: true },
-    refresh_token: { type: [String] }
+    refresh_token: { type: [String], default: [] }
 });
 
 module.exports = model('validasi', skemaValidasi, 'validasi');
