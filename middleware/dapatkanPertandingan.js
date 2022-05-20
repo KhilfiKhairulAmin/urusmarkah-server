96b6c1bba073f8afc737ca3b05d8f470ed43e1d1
iNFiENiTE-World <infienite@gmail.com>
2022-05-20 18:56:35 +0800

    Implementasi perisian tengah dapatkan pertandingan dioptimumkan

--- middleware/dapatkanPertandingan.js
+++ middleware/dapatkanPertandingan.js
@@ -2,24 +2,19 @@ const Pertandingan = require("../model/Pertandingan");
 
 const pengesahanPertandingan = async (req, res, next) => {
     try {
-        const { _id } = req.pengguna;
-        const { pertandingan_id } = req.params;
+        const { _id: pengguna_id } = req.pengguna;
+        const { pertandingan_id: _id } = req.params;
 
         if (!_id) {
             return res.status(500).end();
         }
 
         // Mencari pertandingan dengan id yang diberi
-        const pertandingan = await Pertandingan.findById(pertandingan_id);
+        const pertandingan = await Pertandingan.findOne({ _id, pengguna_id });
 
         // Memastikan pertaningan wujud
         if (!pertandingan) {
             return res.status(404).send({ mesej: 'Pertandingan tidak dijumpai' });
-        }
-    
-        // Memastikan pertandingan adalah milik pengguna
-        if (_id !== pertandingan.pengguna_id) {
-            return res.status(401).send({ mesej: 'Anda tidak dibenarkan mengelola pertandingan ini' });
         }
     
         // Mengumpukkan maklumat pertandingan
