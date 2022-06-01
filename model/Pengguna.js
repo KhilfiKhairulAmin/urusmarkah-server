3f8c4296ff6f8f12d72812ae525a82d8f029444c
iNFiENiTE-World <infienite@gmail.com>
2022-06-02 05:03:24 +0800

    Model Pengguna direka semula

--- model/Pengguna.js
+++ model/Pengguna.js
@@ -1,9 +1,14 @@
 const { Schema, model } = require('mongoose');
 
 const skemaPengguna = new Schema({
-    emel: { type: String, unique: true},
-    nama: { type: String },
-    kata_laluan: { type: String }
+    emel: { type: String, unique: true, required: true},
+    namaAkaun: { type: String, required: true },
+    namaAwal: String,
+    namaAkhir: String,
+    tarikhMasa: {
+        daftar: Date,
+        logMasukTerakhir: Date
+    }
 });
 
 module.exports = model('pengguna', skemaPengguna, 'pengguna');
