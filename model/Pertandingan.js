3ec941fe09cc149e33fcc61ca700642b30fd9711
iNFiENiTE-World <infienite@gmail.com>
2022-06-02 05:18:04 +0800

    Model Pertandingan diubah suai

--- model/Pertandingan.js
+++ model/Pertandingan.js
@@ -1,23 +1,25 @@
 const { Schema, model } = require('mongoose');
 
+const skemaPengiraan = new Schema({
+    caraPengiraanMarkah: { type: String }
+})
+
+const skemaPemilihan = new Schema({
+    caraPemilihanPemenang: { type: String }
+})
 
 const skemaPertandingan = new Schema({
-    pengguna_id: { type: String, required: true },
-    nama_pertandingan: { type: String, required: true },
-    deskripsi: { type: String, default: '' },
-    maklumat_tambahan: { type: Object },
-    konfigurasi: {
-        cara_pengiraan_markah: { type: String, enum: [
-            'Penambahan Tetap', 'Penambahan Dinamik', 'Kriteria'
-        ]},
-        cara_pemilihan_pemenang: { type: String, enum: [
-            'Pertama Meraih N-Markah', 'Markah Tertinggi', 'Kriteria Terbaik', 'Peserta Terakhir'
-        ]}
+    idPengguna: { type: String, required: true },
+    namaPertandingan: { type: String, required: true },
+    deskripsi: { type: String },
+    pengiraan: skemaPengiraan,
+    pemilihan: skemaPemilihan,
+    tarikh: {
+        cipta: Date,
+        laksana: Date,
+        tamat: Date
     },
-    status: { type: Number, enum: [
-        0, 1, 2
-    ], default: 0 },
-    metadata: { tarikh_dibuat: Date, tarikh_berlangsung: Date, tarikh_tamat: Date }
+    bilPusingan: Number
 });
 
 module.exports = model('pertandingan', skemaPertandingan, 'pertandingan');
