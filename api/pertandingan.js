16b81ae3dd305f6d3d7cc6f2031be5e1a3b8d92f
iNFiENiTE-World <infienite@gmail.com>
2022-06-10 14:37:45 +0800

    Route pertandingan dikemaskini dengan baik. Peserta mengalami perubahan kecil

--- api/pertandingan.js
+++ api/pertandingan.js
@@ -1,150 +1,160 @@
 const express = require('express');
 const dapatkanPertandingan = require('../middleware/dapatkanPertandingan');
+const Penyingkiran = require('../model/Penyingkiran');
 const Pengiraan = require('../model/Pengiraan');
 const Pertandingan = require('../model/Pertandingan');
-const Peserta = require('../model/Peserta');
+const penjanaTarikhMasa = require('../util/janaTarikhMasa');
 const router = express.Router();
+const kendaliRalatMongoose = require('../util/kendaliRalatMongoose');
+const Ralat = require('../util/Ralat');
 
-router.post('/cipta', (req, res) => {
+router.post('/cipta', async (req, res) => {
     try {
-        const { namaPertandingan } = req.body;
-
-        // Memastikan nama pertandingan wujud
-        if (!namaPertandingan) {
-            return res.status(400).send({ mesej: 'Perlukan nama pertandingan' });
-        }
-    
-        const { deskripsi, pengiraan: kira, pemilihan: pilih } = req.body;
+        const { nama, pengiraan, penyingkiran } = req.body;
         const { pengelola } = req.muatanToken;
 
-        const pengiraan = await Pengiraan.findById(kira);
+        const pengiraanWujud = await Pengiraan.findOne({ noRujukan: pengiraan }, '_id');
 
-        if (!pengiraan) {
-            return res.status(400).send({ mesej: 'Sila pastikan nombor pengiraan sah'})
-        }
+        if (!pengiraanWujud) throw new Ralat('pengiraan', 'Jenis Pengiraan tidak wujud');
+
+        const penyingkiranWujud = await Penyingkiran.findOne({ noRujukan: penyingkiran }, '_id');
+
+        if (!penyingkiranWujud) throw new Ralat('penyingkiran', 'Jenis Penyingkiran tidak wujud');
+
+        if (nama.length > 255) throw new Ralat('nama', 'Nama pertandingan tidak boleh melebihi 255 perkataan');
 
         // Mencipta pertandingan baharu
         const pertandingan = new Pertandingan({
             pengelola,
-            namaPertandingan,
-            deskripsi,
-            konfigurasi,
-            metadata: {
-                tarikh_dibuat: formatTarikh(new Date())
+            nama,
+            pengiraan: pengiraanWujud._id,
+            penyingkiran: penyingkiranWujud._id,
+            tarikhMasa: {
+                cipta: penjanaTarikhMasa()
             }
         });
-        
-        // Memastikan maklumat yang dimasukkan sesuai dengan skema pertandingan
-        const error = pertandingan.validateSync();
-
-        if (error) {
-            console.log(error);
-            return res.status(400).send({ mesej: 'Sila periksa semula maklumat yang diberi' });
-        }
 
         // Menyimpan maklumat pertandingan dalam pangkalan data
-        pertandingan.save();
-    
-        res.redirect(`./${pertandingan._id}`, 302)
-    } catch (err) {
-        console.log(err);
-        return res.status(500).send({ mesej: 'Ralat dalaman server' });
+        await pertandingan.save();
+
+        res.status(201).send({ pertandingan: pertandingan._id });
+
+    } catch (ralat) {
+        kendaliRalatMongoose(res, ralat, 'Sila pastikan butiran pertandingan mengikut format');
     }
-});
+}); 
 
 router.get('/', async (req, res) => {
     try {
-        const { pengguna } = req;
+        const { pengelola } = req.muatanToken;
 
-        const pertandingan = await Pertandingan.find({ pengguna_id: pengguna._id }, 'nama_pertandingan deskripsi status metadata');
+        const semuaPertandingan = await Pertandingan.find({ pengelola }, 'nama tarikhMasa');
     
-        res.status(200).send(pertandingan);
+        res.status(200).send(semuaPertandingan);
 
-    } catch (err) {
-        console.log(err);
-
-        res.status(500).end();
+    } catch (ralat) {
+        kendaliRalatMongoose(res, ralat, 'Pertandingan tidak dijumpai');
     }
 });
 
-router.get('/:pertandingan_id', dapatkanPertandingan, (req, res) => {
+router.get('/:pertandingan', async (req, res) => {
     try {
-        const { pertandingan } = req;
+        const { pertandingan: _id } = req.params;
+        const { pengelola } = req.muatanToken;
+
+        const pertandingan = await Pertandingan.findOne({ _id, pengelola }, '-pengelola -_id').populate('pengiraan', '-_id').populate('penyingkiran', '-_id');
+
+        if (!pertandingan) throw new Ralat('Pencarian', 'Pertandingan tidak wujud' );
 
         // Mengembalikan pertandingan
         res.status(200).send(pertandingan);
 
-    } catch (err) {
-        console.log(err);
-
-        res.status(500).end();
+    } catch (ralat) {
+        kendaliRalatMongoose(res, ralat, 'Pertandingan tidak dijumpai')
     }
 });
 
-router.put('/:pertandingan_id/kemas_kini', dapatkanPertandingan, (req, res) => {
+router.put('/:pertandingan/kemas_kini', async (req, res) => {
     try {
-        const { pertandingan } = req;
-        const { nama_pertandingan } = req.body;
-    
-        // Memastikan nama pertandingan tidak kosong
-        if (!nama_pertandingan) {
-            return res.status(400).send({ mesej: 'Sila berikan nama pertandingan'});
-        }
-    
-        const { deskripsi, maklumat_tambahan, konfigurasi } = req.body;
-    
-        // Mengemaskini nama pertandingan (dengan cara overwrite)
-        pertandingan.nama_pertandingan = nama_pertandingan;
+        const { pertandingan: _id } = req.params;
+        const { pengelola } = req.muatanToken;
+        const { nama, deskripsi, tarikhPelaksanaan, syarat, sumber } = req.body;
 
-        // Mengemaskini maklumat lain pertandingan
-        pertandingan.deskripsi = deskripsi ?? pertandingan.deskripsi;
-        pertandingan.maklumat_tambahan = maklumat_tambahan ?? pertandingan.maklumat_tambahan;
-        pertandingan.konfigurasi.cara_pengiraan_markah = konfigurasi.cara_pengiraan_markah ?? pertandingan.konfigurasi.cara_pengiraan_markah;
-        pertandingan.konfigurasi.cara_pemilihan_pemenang = konfigurasi.cara_pemilihan_pemenang ?? pertandingan.konfigurasi.cara_pemilihan_pemenang;
+        const pertandingan = await Pertandingan.findOne({ _id, pengelola }, 'nama tentang');
+
+        if (!pertandingan) throw new Ralat('Pencarian', 'Pertandingan tidak dijumpai');
+
+        if (nama && nama.length > 255) throw new Ralat('nama', 'Nama pertandingan tidak boleh melebihi 255 perkataan');
+
+        if (deskripsi && deskripsi.length > 511) throw new Ralat('deskripsi', 'Deskripsi pertandingan tidak boleh melebihi 511 perkataan');
+
+        if (syarat && !Array.isArray(syarat)) throw new Ralat('syarat', 'Syarat-syarat perlu diletakkan dalam Array')
+
+        if (sumber && !Array.isArray(sumber)) throw new Ralat('sumber', 'Sumber-sumber perlu diletakkan dalam Array')
+
+        const { tentang } = pertandingan;
+
+        tentang.deskripsi = deskripsi || tentang.deskripsi;
+        tentang.tarikhPelaksanaan = tarikhPelaksanaan || tentang.tarikhPelaksanaan;
+        tentang.syarat = syarat || tentang.syarat;
+        tentang.sumber = sumber || tentang.sumber;
+
+        await pertandingan.save();
+
+        // res.status(200).send({ mesej: 'Tetapan berjaya dikemaskini'});
+        res.status(200).send({ mesej: 'Pertandingan berjaya dikemaskini'});
+
+        // // Mengemaskini nama pertandingan (dengan cara overwrite)
+        // pertandingan.nama_pertandingan = nama_pertandingan;
+
+        // // Mengemaskini maklumat lain pertandingan
+        // pertandingan.deskripsi = deskripsi ?? pertandingan.deskripsi;
+        // pertandingan.maklumat_tambahan = maklumat_tambahan ?? pertandingan.maklumat_tambahan;
+        // pertandingan.konfigurasi.cara_pengiraan_markah = konfigurasi.cara_pengiraan_markah ?? pertandingan.konfigurasi.cara_pengiraan_markah;
+        // pertandingan.konfigurasi.cara_pemilihan_pemenang = konfigurasi.cara_pemilihan_pemenang ?? pertandingan.konfigurasi.cara_pemilihan_pemenang;
     
-        // Memastikan maklumat yang dikemaskini sesuai dengan skema pertandigan
-        const error = pertandingan.validateSync();
+        // // Memastikan maklumat yang dikemaskini sesuai dengan skema pertandigan
+        // const error = pertandingan.validateSync();
     
-        if (error) {
-            return res.status(400).send({ mesej: 'Sila periksa semula maklumat yang diberi' });
-        }
+        // if (error) {
+        //     return res.status(400).send({ mesej: 'Sila periksa semula maklumat yang diberi' });
+        // }
     
-        // Menyimpan maklumat pertandingan dalam pangkalan data
-        pertandingan.save();
+        // // Menyimpan maklumat pertandingan dalam pangkalan data
+        // pertandingan.save();
     
-        res.status(200).send({ mesej: 'Kemaskini berjaya' });
-    } catch (err) {
-        console.log(err);
-        res.status(500).end();
+        // res.status(200).send({ mesej: 'Kemaskini berjaya' });
+    } catch (ralat) {
+        kendaliRalatMongoose(res, ralat, 'Sila pastikan butiran tentang pertandingan mengikut format')
     }
 });
 
-router.delete('/:pertandingan_id/hapus', dapatkanPertandingan, async (req, res) => {
-    const { pertandingan } = req;
-    const { nama_pertandingan: pengesahan } = req.body;
-
-    // Memastikan pengesahan diberi
-    // Pengesahan ialah nama pertandingan yang hendak dihapuskan
-    if (!pengesahan) {
-        return res.status(403).send({ mesej: 'Sila berikan nama pertandingan' });
+router.delete('/:pertandingan/hapus', async (req, res) => {
+    try {
+        const { pertandingan: _id } = req.params;
+        const { pengelola } = req.muatanToken;
+        const { nama } = req.body;
+    
+        // Memastikan pengesahan diberi
+        // Pengesahan ialah nama pertandingan yang hendak dihapuskan
+        if (!nama) throw new Ralat('nama', 'Sila berikan nama pertandingan sebagai pengesahan');
+    
+        const pertandingan = await Pertandingan.findOne({ _id, pengelola }, 'nama');
+    
+        if (!pertandingan) throw new Ralat('Pencarian', 'Pertandingan tidak dijumpai');
+    
+        if (!nama) throw new Ralat('nama', 'Sila berikan nama pertandingan sebagai pengesahan');
+    
+        if (nama !== pertandingan.nama) throw new Ralat('Nama', 'Nama pertandingan tidak tepat. Pertandingan gagal dihapus.');
+    
+        const { deletedCount: hapus } = await Pertandingan.deleteOne({ _id });
+    
+        if (!hapus) throw new Ralat('Hapus', 'Pertandingan gagal dihapuskan');
+    
+        res.status(200).send({ mesej: 'Pertandingan berjaya dihapuskan'});
+    } catch (ralat) {
+        kendaliRalatMongoose(res, ralat, 'Sila pastikan butiran mengikut format betul')
     }
-
-    // Memastikan pengesahan yang diberi betul
-    if (pengesahan !== pertandingan.nama_pertandingan) {
-        return res.status(403).send({ mesej: 'Pertandingan tidak berjaya dihapuskan' });
-    }
-
-    // Menghapuskan maklumat pertandingan
-    await pertandingan.deleteOne({ _id: pertandingan._id})
-    .catch((err) => {
-        console.log(err);
-        return res.status(500).end();
-    });
-
-    // Menghapuskan maklumat peserta dalam pertandingan
-    const { deletedCount: bilPesertaHapus } = await Peserta.deleteMany({ pertandingan_id: pertandingan._id });
-
-    res.status(200).send({ mesej: `1 Pertandingan telah dihapuskan. ${bilPesertaHapus} Peserta telah dihapuskan` });
 });
 
 // Route peserta
--- api/peserta.js
+++ api/peserta.js
@@ -1,6 +1,5 @@
 const { hash, compare } = require('bcryptjs');
 const express = require('express');
-const pengesahanPeserta = require('../middleware/pengesahanPeserta');
 const Peserta = require('../model/Peserta');
 const Session = require('../model/Session');
 const router = express.Router();
@@ -127,71 +126,6 @@ router.put ('/kemas_kini', async (req, res) => {
 
         res.status(200).send(peserta);
 
-    } catch (err) {
-        console.log(err);
-        res.status(500).end();
-    }
-});
-
-router.put('/:peserta_id/kemas_kini', pengesahanPeserta, async (req, res) => {
-    try {
-        const { peserta } = req;
-        const { nama_peserta } = req.body;
-
-        // Memastikan nama peserta diberikan
-        if (!nama_peserta) {
-            return res.status(400).send({ mesej: 'Sila berikan nama peserta' });
-        }
-
-        // Mengemaskini nama peserta (dengan cara overwrite)
-        peserta.nama_peserta = nama_peserta;
-
-        // Menyimpan maklumat dalam pangkalan data
-        peserta.save();
-
-        res.status(200).send(peserta);
-    } catch (err) {
-        console.log(err);
-        res.status(500).end();
-    }
-});
-
-router.delete('/hapus', async (req, res) => {
-    try {
-        const { peserta_id } = req.body;
-        
-        // Memastikan id peserta diberikan
-        if (!peserta_id) {
-            res.status(400).send({ mesej: 'Sila berikan ID Peserta' });
-        }
-
-        // Memastikan maklumat peserta diletakkan dalam Array
-        // Ini kerana aplikasi ini tidak menyokong hapus satu peserta, hanya hapus banyak dibenarkan
-        if (!Array.isArray(peserta_id)) {
-            return res.status(400).send({ mesej: 'Sila letakkan peserta dalam Array' });
-        }
-
-        let i = 1;
-
-        for (const id of peserta_id) {
-
-            // Memastikan id peserta diberikan
-            if (!id) {
-                return res.status(400).send({ mesej: `Sila letakkan ID Peserta pada peserta ${i}` });
-            }
-
-            // Memastikan format id peserta adalah string
-            if (typeof id !== 'string') {
-                return res.status(400).send({ mesej: `Sila letakkan ID dalam format string pada peserta ${i}` });
-            }
-
-            i++;
-        }
-
-        // Menghapuskan peserta berkenaan dalam pangkalan data
-        const { deletedCount } = await Peserta.deleteMany({ _id: peserta_id });
-
-        res.status(200).send({ mesej: `${deletedCount} peserta telah dihapuskan` });
     } catch (err) {
         console.log(err);
         res.status(500).end();
