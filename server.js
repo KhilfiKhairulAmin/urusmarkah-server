673351d5d95a94411e80f379baf75e94e78d0aac
iNFiENiTE-World <infienite@gmail.com>
2023-06-18 17:49:16 +0800

    Log server port

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
 server.listen(port, () => {
-    // console.log(`Mendengar pada port:${port}`);
+    console.log(`Mendengar pada port:${port}`);
 });
